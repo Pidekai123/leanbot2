c670a9aa3235bf291c0e643418cf6a6a9278dbef
Pidekai123 <rubenstahlhaupt@gmail.com>
2023-05-30 15:01:10 +0200

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this document?',
+        message: 'Hi I am Leanbot how can i help you?',
         type: 'apiMessage',
       },
     ],
