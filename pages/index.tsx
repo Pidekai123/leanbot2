2d49516c99afe83325dfc62dea90ffffa454c986
Pidekai123 <rubenstahlhaupt@gmail.com>
2023-05-30 15:05:37 +0200

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -224,7 +224,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : 'How can i help you?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
